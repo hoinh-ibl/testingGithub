6632fd8516fc2942b64f2a1b150be97e718b6aa4
Hoi Nguyen <hoinh@cardano-labo.com>
2017-04-07 16:58:35 +0900

    ref trong reactJs

--- public/controller.js
+++ public/controller.js
@@ -15,12 +15,12 @@ var DemoReact = React.createClass({
   render: function(){
     return(
       <div>
-        <h1>State in the reactJs session 2</h1>
+        <h1>Ref in the reactJs</h1>
         <h1 className="backgroundYellow">{this.props.name} - {this.props.professor} </h1>
         <div>so hoc vien:{this.state.totalAttendent}</div>
         <p>{this.props.children}</p>
-        <button onClick={()=>{var str = this.props.name + " " + this.props.professor; getName(str)}}>info name professor</button>
-        <button onClick={this.addStudent}>info student</button>
+        <button onClick={()=>{var str = this.props.name + " " + this.props.professor; getName(str)}}>info professor</button>
+        <button onClick={this.addStudent}>info Student</button>
         <Course />
       </div>
     );
@@ -36,8 +36,28 @@ var Course = React.createClass({
   }
 });
 
+var InputTag = React.createClass({
+  show(){
+    var text = this.refs.selected.value;
+    alert(text);
+  },
+  render(){
+    return(
+        <div>
+          <select ref="selected">
+            <option value="a">anpha</option>
+            <option value="b">beta</option>
+            <option value="g">gamma</option>
+          </select>
+          <input type="text" ref="txt"></input>
+          <button onClick={this.show}>Show  </button>
+        </div>
+    );
+  }
+});
 ReactDOM.render(
   <div>
+    <InputTag/>
     <DemoReact name="ReactJs" professor="Alex" totalAttendent="10"> the course ReactJs</DemoReact>
     <hr/>
     <DemoReact name="NodeJs" professor="Kozomi" totalAttendent="20"> the course NodeJs</DemoReact>
