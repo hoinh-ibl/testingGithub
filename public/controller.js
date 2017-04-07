7613187bf0c4bb9ed361e9eddd01df5a1ea86cb7
Hoi Nguyen <hoinh@cardano-labo.com>
2017-04-07 15:23:41 +0900

    Tach react ra file rieng

---
+++ public/controller.js
@@ -0,0 +1,14 @@
+ReactDOM.render(
+  //React.createElement("h1", {}, "Demo JSX library"),
+  <div>
+    <h1>Clone react to file</h1>
+    <h1 className="backgroundYellow">Demo clone react to file</h1>
+  </div>
+  ,document.getElementById("root"));
+
+  ReactDOM.render(
+    <div>
+      <h1 className="backgroundYellow">Demo reactJS view 1</h1>
+      <h1 className="backgroundYellow">Demo reactJS view 2</h1>
+    </div>
+    ,document.getElementById("root2"));
