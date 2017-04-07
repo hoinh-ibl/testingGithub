727dfebe26e029532d0ecf7f206c483ddf610ac2
Hoi Nguyen <hoinh@cardano-labo.com>
2017-04-07 16:02:40 +0900

    bat su kien trong reactJs phan 1

--- public/controller.js
+++ public/controller.js
@@ -1,10 +1,17 @@
+function getName(name){
+  alert(name);
+}
 var DemoReact = React.createClass({
+  getInfo(){
+    alert(this.props.children);
+  },
   render: function(){
     return(
       <div>
-        <h1>Demo props children in reactJs</h1>
+        <h1>Event handler in reactJs session 1</h1>
         <h1 className="backgroundYellow">{this.props.name} - {this.props.professor} </h1>
         <p>{this.props.children}</p>
+        <button onClick={()=>{var str = this.props.name + " " + this.props.professor; getName(str)}}>info</button>
         <Course />
       </div>
     );
